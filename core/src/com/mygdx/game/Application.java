26d634928fea0d9dd06f3cc6878f3856cbaf18f0
d.saraev <black8.tabls@gmail.com>
2022-08-17 02:23:04 +0300

    ninja and background

--- core/src/com/mygdx/game/Application.java
+++ core/src/com/mygdx/game/Application.java
@@ -12,12 +12,12 @@ public class Application extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("ninja.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 1, 1, 0.5f);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
