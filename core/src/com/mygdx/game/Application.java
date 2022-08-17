d7ee0dec39de7d493ea8ff439ad9825b1c201c95
d.saraev <black8.tabls@gmail.com>
2022-08-18 01:57:34 +0300

    correct mouse following

--- core/src/com/mygdx/game/Application.java
+++ core/src/com/mygdx/game/Application.java
@@ -2,34 +2,44 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Application extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("ninja.png");
-	}
+    SpriteBatch batch;
+    Texture img;
+    int click;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(0, 1, 1, 0.5f);
-		float x = Gdx.input.getX();
-		float y = Gdx.graphics.getHeight() - Gdx.input.getY();
-		batch.begin();
-		batch.draw(img, 0, 0 );
-		batch.draw(img, x, y);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("ninja.png");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 1, 1, 0.5f);
+        float x = Gdx.input.getX();
+        float y = Gdx.graphics.getHeight() - Gdx.input.getY();
+
+        float middleOfImgY = img.getHeight() / 2.0f;
+        float middleOfImgX = img.getWidth() / 2.0f;
+
+        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) click++;
+
+        Gdx.graphics.setTitle("Was clicked: " + click + " times! :)");
+
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.draw(img, x - middleOfImgX, y - middleOfImgY);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -6,10 +6,9 @@ import com.mygdx.game.Application;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("My GDX Game");
-		new Lwjgl3Application(new Application(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        new Lwjgl3Application(new Application(), config);
+    }
 }
